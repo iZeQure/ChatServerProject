645864f24d6c24979b0f90e4621afff798502dac
goket1 <goket@protonmail.com>
2020-11-04 15:09:54 +0100

    Created a proof of concept on the socket listener

--- Majesty/Program.cs
+++ Majesty/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,6 +10,9 @@ namespace Majesty
 {
     class Program
     {
+        // Incoming data from the client.  
+        public string data = null;  
+        
         static void Main(string[] args) => 
             new Program()
             .StartServerAsync()
@@ -15,7 +21,7 @@ namespace Majesty
 
         private async Task StartServerAsync()
         {
-            Thread simpleThread = new Thread(SimpleListener);
+            new Thread(SimpleListener).Start();
             
             Console.WriteLine("Hello World!");
 
@@ -24,7 +30,67 @@ namespace Majesty
 
         void SimpleListener()
         {
+                    // Data buffer for incoming data.  
+        byte[] bytes = new Byte[1024];  
+  
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the
+        // host running the application.  
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());  
+        IPAddress ipAddress = ipHostInfo.AddressList[0];  
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);  
+  
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,  
+            SocketType.Stream, ProtocolType.Tcp );  
+  
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try {  
+            listener.Bind(localEndPoint);  
+            listener.Listen(10);  
+  
+            // Start listening for connections.  
+            while (true) {  
+                Console.WriteLine("Waiting for a connection...");  
+                // Program is suspended while waiting for an incoming connection.  
+                Socket handler = listener.Accept();
 
+                _ = Task.Run(() =>
+                {
+                    Console.WriteLine("Connected");
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.  
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                });
+                Console.WriteLine("ASs hat");
+            }  
+  
+        } catch (Exception e) {  
+            Console.WriteLine(e.ToString());  
+        }  
+  
+        Console.WriteLine("\nPress ENTER to continue...");  
+        Console.Read();  
         }
     }
 }
